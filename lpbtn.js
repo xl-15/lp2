f6cf846ab9c3391223cda4790e81084459d327e8
xl-15 <lewv32@gmail.com>
2024-10-22 02:22:02 -0700

    search bar made and js woring, next up is to change the contect layout, font size and font fam. ggs'

--- lpbtn.js
+++ lpbtn.js
@@ -7,3 +7,33 @@ function hideSideBar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+
+
+
+function showSearchTab(){
+    const search = document.querySelector('.searchInput')
+    search.style.display = 'flex'
+
+     const searchIcon = document.querySelector('.searchIcon')
+    searchIcon.style.display = 'none'
+
+    const closeIcon = document.querySelector('.closeIcon')
+    closeIcon.style.display = 'flex'
+
+}
+
+function closeSearchTab(){
+    const closeSearch = document.querySelector('.closeIcon')
+    closeSearch.style.display = 'none'
+
+    const search = document.querySelector('.searchInput')
+    search.style.display = 'none'
+
+    const searchIcon = document.querySelector('.searchIcon')
+    searchIcon.style.display = 'flex'
+
+
+
+    
+}
